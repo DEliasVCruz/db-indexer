f854595c84ad26e06e140cc6b04a635a01ae230a
DEliasVilelaC <danielvilelayo@gmail.com>
2022-12-19 22:21:42 -0500

    chore: reword check log messages

--- cmd/db-indexer/main.go
+++ cmd/db-indexer/main.go
@@ -75,15 +75,15 @@ func check(check string, err error) {
 	if err != nil {
 		switch check {
 		case "requestCreation":
-			log.Fatalf("client: could not create request: %s\n", err)
+			log.Fatalf("client: could not create request with %s\n", err)
 		case "fileOpen":
-			log.Fatalf("file: could not open given file: %s\n", err)
+			log.Fatalf("file: could not open given file with %s\n", err)
 		case "requestAction":
-			log.Fatalf("client: error making http request: %s\n", err)
+			log.Fatalf("client: error making http request with %s\n", err)
 		case "responseRead":
-			log.Fatalf("client: could not read response body: %s\n", err)
+			log.Fatalf("client: could not read response body with %s\n", err)
 		default:
-			log.Fatal(err)
+			log.Fatalf("error: something went wrong with %s\n", err)
 		}
 	}
 }
