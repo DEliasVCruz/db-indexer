831ee6a3f121c0ddd81440c0b0c1f6ad95e3236b
DEliasVilelaC <danielvilelayo@gmail.com>
2022-12-20 21:22:08 -0500

    refactor: extract file indexing logic

--- cmd/db-indexer/main.go
+++ cmd/db-indexer/main.go
@@ -95,6 +95,74 @@ var data string
 
 var mainDir = "enron_mail_20110402/maildir"
 
+func dataExtract(path string) {
+	log.Printf("file: reading file path %s", path)
+	input, err := os.Open(path)
+	check("fileOpen", err)
+	defer input.Close()
+
+	var fields = map[string]string{
+		"message_id":                "",
+		"date":                      "",
+		"from":                      "",
+		"to":                        "",
+		"subject":                   "",
+		"cc":                        "",
+		"mime_version":              "",
+		"content_type":              "",
+		"charset":                   "",
+		"content_transfer_encoding": "",
+		"bcc":                       "",
+		"x_from":                    "",
+		"x_to":                      "",
+		"x_cc":                      "",
+		"x_bcc":                     "",
+		"x_folder":                  "",
+		"x_origin":                  "",
+		"x_filename":                "",
+		"contents":                  "",
+	}
+
+	scanner := bufio.NewScanner(input)
+
+	fieldRegex, _ := regexp.Compile(`^([\w\-]*): (.*)`)
+	brokenLineRegex, _ := regexp.Compile(`^\s*(.*)\s*$`)
+	regexMessage, _ := regexp.Compile(`^<(\d+\.\d+)\..*`)
+	metadataInfo := true
+	for scanner.Scan() {
+		line := scanner.Text()
+		if metadataInfo {
+			if strings.TrimSpace(line) == "" {
+				metadataInfo = false
+			} else if fieldRegex.MatchString(line) {
+				match := fieldRegex.FindStringSubmatch(line)
+				field = strings.ReplaceAll(strings.ToLower(match[1]), "-", "_")
+				data = strings.TrimSpace(match[2])
+				fields[field] = strings.TrimSpace(data)
+			} else {
+				data = brokenLineRegex.FindStringSubmatch(line)[1]
+				fields[field] += fmt.Sprintf(" %s", strings.TrimSpace(data))
+			}
+		} else {
+			fields["contents"] += fmt.Sprintf("%s\n", line)
+		}
+	}
+
+	fields["message_id"] = regexMessage.FindStringSubmatch(fields["message_id"])[1]
+	contentTypes := strings.Split(fields["content_type"], ";")
+	fields["content_type"] = contentTypes[0]
+	fields["charset"] = strings.Split(contentTypes[1], "=")[1]
+	fields["x_folder"] = strings.ReplaceAll(fields["x_folder"], "\\", "/")
+
+	jsonPayLoad, _ := json.Marshal(fields)
+	status, respBody := request(http.MethodPost, fmt.Sprintf("%s/_doc", defaultIndex), jsonPayLoad)
+	if status == 200 {
+		log.Printf("client: succsefull response with status code %d", status)
+		log.Printf("client: response body %s", respBody)
+	}
+
+}
+
 func fsWalker(childPath string, dir fs.DirEntry, err error) error {
 	fullPath := filepath.Join(mainDir, childPath)
 	if err != nil {
@@ -103,70 +171,7 @@ func fsWalker(childPath string, dir fs.DirEntry, err error) error {
 	}
 
 	if !dir.IsDir() {
-		input, err := os.Open(fullPath)
-		check("fileOpen", err)
-		defer input.Close()
-
-		var fields = map[string]string{
-			"message_id":                "",
-			"date":                      "",
-			"from":                      "",
-			"to":                        "",
-			"subject":                   "",
-			"cc":                        "",
-			"mime_version":              "",
-			"content_type":              "",
-			"charset":                   "",
-			"content_transfer_encoding": "",
-			"bcc":                       "",
-			"x_from":                    "",
-			"x_to":                      "",
-			"x_cc":                      "",
-			"x_bcc":                     "",
-			"x_folder":                  "",
-			"x_origin":                  "",
-			"x_filename":                "",
-			"contents":                  "",
-		}
-
-		scanner := bufio.NewScanner(input)
-
-		fieldRegex, _ := regexp.Compile(`^([\w\-]*): (.*)`)
-		brokenLineRegex, _ := regexp.Compile(`^\s*(.*)\s*$`)
-		regexMessage, _ := regexp.Compile(`^<(\d+\.\d+)\..*`)
-		metadataInfo := true
-		for scanner.Scan() {
-			line := scanner.Text()
-			if metadataInfo {
-				if strings.TrimSpace(line) == "" {
-					metadataInfo = false
-				} else if fieldRegex.MatchString(line) {
-					match := fieldRegex.FindStringSubmatch(line)
-					field = strings.ReplaceAll(strings.ToLower(match[1]), "-", "_")
-					data = strings.TrimSpace(match[2])
-					fields[field] = strings.TrimSpace(data)
-				} else {
-					data = brokenLineRegex.FindStringSubmatch(line)[1]
-					fields[field] += fmt.Sprintf(" %s", strings.TrimSpace(data))
-				}
-			} else {
-				fields["contents"] += fmt.Sprintf("%s\n", line)
-			}
-		}
-
-		fields["message_id"] = regexMessage.FindStringSubmatch(fields["message_id"])[1]
-		contentTypes := strings.Split(fields["content_type"], ";")
-		fields["content_type"] = contentTypes[0]
-		fields["charset"] = strings.Split(contentTypes[1], "=")[1]
-		fields["x_folder"] = strings.ReplaceAll(fields["x_folder"], "\\", "/")
-
-		jsonPayLoad, _ := json.Marshal(fields)
-		status, respBody := request(http.MethodPost, fmt.Sprintf("%s/_doc", defaultIndex), jsonPayLoad)
-		if status == 200 {
-			log.Printf("client: succsefull response with status code %d", status)
-			log.Printf("client: response body %s", respBody)
-		}
-
+		dataExtract(fullPath)
 	}
 
 	return nil
@@ -175,5 +180,5 @@ func fsWalker(childPath string, dir fs.DirEntry, err error) error {
 func main() {
 	createIndex()
 	fsys := os.DirFS(mainDir)
-	fs.WalkDir(fsys, ".", fileIndexing)
+	fs.WalkDir(fsys, ".", fsWalker)
 }
