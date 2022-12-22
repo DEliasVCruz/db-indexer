4dc4769ade687e1311e41831c13f151593036cd3
DEliasVilelaC <danielvilelayo@gmail.com>
2022-12-21 21:59:42 -0500

    refactor: extract index logic into module struct

--- main.go
+++ main.go
@@ -1,49 +1,16 @@
 package main
 
 import (
-	"io/fs"
-	"log"
 	"os"
 	"path/filepath"
+
+	"github.com/DEliasVCruz/db-indexer/pkg/index"
 )
 
-var mainDir = filepath.Join(os.Args[1], "maildir")
-
-func mailDirIndex(mailDir fs.FS, rootPath string) {
-	fs.WalkDir(mailDir, rootPath, indexWalker)
-	if len(records) != 0 {
-		createDocBatch(records)
-		records = nil
-	}
-}
-
-var records = []map[string]string{}
-
-func indexWalker(childPath string, dir fs.DirEntry, err error) error {
-	fullPath := filepath.Join(mainDir, childPath)
-	if err != nil {
-		log.Printf("error: when attempting to read file %s raised %s", fullPath, err)
-		return nil
-	}
-
-	if !dir.IsDir() {
-		fields, err := dataExtract(fullPath)
-		if err != nil {
-			log.Printf("error: %s", err)
-			return nil
-		}
-		records = append(records, fields)
-	}
-
-	if len(records) == 100 {
-		createDocBatch(records)
-		records = nil
-	}
-
-	return nil
-}
-
 func main() {
-	createIndex()
-	mailDirIndex(os.DirFS(mainDir), "bailey-s/inbox")
+	indexer := index.Indexer{
+		Name:       "emailsTest",
+		DataFolder: filepath.Join(os.Args[1], "maildir/bailey-s/inbox"),
+	}
+	indexer.Index()
 }
---
+++ pkg/index/indexer.go
@@ -0,0 +1,56 @@
+package index
+
+import (
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/DEliasVCruz/db-indexer/pkg/data"
+	"github.com/DEliasVCruz/db-indexer/pkg/zinc"
+)
+
+type Indexer struct {
+	Name       string
+	DataFolder string
+}
+
+func (i Indexer) Index() {
+	zinc.CreateIndex(i.Name)
+	i.indexDirFiles(os.DirFS(i.DataFolder))
+}
+
+func (i Indexer) indexDirFiles(directory fs.FS) {
+
+	var records = []map[string]string{}
+
+	fs.WalkDir(directory, ".", func(childPath string, dir fs.DirEntry, err error) error {
+
+		fullPath := filepath.Join(i.DataFolder, childPath)
+		if err != nil {
+			log.Printf("error: when attempting to read file %s raised %s", fullPath, err)
+			return nil
+		}
+
+		if !dir.IsDir() {
+			fields, err := data.Extract(fullPath)
+			if err != nil {
+				log.Printf("error: %s", err)
+				return nil
+			}
+			records = append(records, fields)
+		}
+
+		if len(records) == 100 {
+			zinc.CreateDocBatch(i.Name, records)
+			records = nil
+		}
+
+		return nil
+	})
+
+	if len(records) != 0 {
+		zinc.CreateDocBatch(i.Name, records)
+		records = nil
+	}
+}
