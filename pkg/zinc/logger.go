c7054149ebf858d34ee52e1fbdd8cbe26ab1509d
DEliasVilelaC <danielvilelayo@gmail.com>
2022-12-26 15:55:53 -0500

    refactor: remove logger struct
    add dataProcess channel

--- pkg/zinc/logger.go
+++ pkg/zinc/logger.go
@@ -1,19 +1,15 @@
 package zinc
 
-type Logger struct {
-	Name string
-}
-
-func (l Logger) LogInfo(message string) {
-	CreateDoc(l.Name, map[string]string{
+func LogInfo(message string) {
+	CreateDoc("appLogs", map[string]string{
 		"severity": "info",
 		"message":  message,
 	})
 
 }
 
-func (l Logger) LogError(message, err string) {
-	CreateDoc(l.Name, map[string]string{
+func LogError(message, err string) {
+	CreateDoc("appLogs", map[string]string{
 		"severity": "error",
 		"message":  message,
 		"error":    err,
