b5b1481ffc6c5b8afb29c22f2d64de1b8ac6cfd5
DEliasVilelaC <danielvilelayo@gmail.com>
2022-12-26 16:46:31 -0500

    feat: add logging index creation

--- main.go
+++ main.go
@@ -6,11 +6,14 @@ import (
 
 	"github.com/DEliasVCruz/db-indexer/pkg/check"
 	"github.com/DEliasVCruz/db-indexer/pkg/index"
+	"github.com/DEliasVCruz/db-indexer/pkg/zinc"
 )
 
 func main() {
 	indexConfig, err := os.ReadFile("./index.json")
 	check.Error("fileOpen", err)
+
+	zinc.CreateIndex("appLogs", nil)
 
 	indexer := index.Indexer{
 		Name:       "emailsTest",
