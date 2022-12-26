6c2c7126e339e33956eb23c3324f21a470da31ba
DEliasVilelaC <danielvilelayo@gmail.com>
2022-12-26 02:56:29 -0500

    feat: extract constant byte variables

--- pkg/data/data.go
+++ pkg/data/data.go
@@ -16,32 +16,38 @@ var fieldRegex, _ = regexp.Compile(`^([\w\-]*): (.*)`)
 var brokenLineRegex, _ = regexp.Compile(`^\s*(.*)\s*$`)
 var messageRegex, _ = regexp.Compile(`^<(\d+\.\d+)\..*`)
 
+var fieldBreakBefore, fieldBreakAfter = []byte("-"), []byte("_")
+var fieldMetadataFlag, emptyByte = []byte("x_filename"), []byte("")
+var dataJoin, dataNewLine = []byte(" "), []byte("\n")
+var contentTypeSep, charsetSep = []byte(";"), []byte("=")
+var oldPathSep, newPathSep = []byte(`\`), []byte("/")
+
 func Extract(path string) (map[string][]byte, error) {
 	log.Printf("file: reading file path %s", path)
 	input, err := os.Open(path)
 	check.Error("fileOpen", err)
 	defer input.Close()
 
-	fields := map[string][]byte{}
-	field := []byte("")
+	fields := make(map[string][]byte)
+	var field []byte
 
 	allMetadataParsed := false
 	scanner := bufio.NewScanner(input)
 	for scanner.Scan() {
 		line := scanner.Bytes()
 		if !allMetadataParsed {
-			if bytes.Equal(field, []byte("x_filename")) {
+			if bytes.Equal(field, fieldMetadataFlag) {
 				allMetadataParsed = true
 			} else if match := fieldRegex.FindSubmatch(line); match != nil {
-				field = bytes.ReplaceAll(bytes.ToLower(match[1]), []byte("-"), []byte("_"))
+				field = bytes.ReplaceAll(bytes.ToLower(match[1]), fieldBreakBefore, fieldBreakAfter)
 				fields[string(field)] = bytes.TrimSpace(match[2])
 			} else {
 				byteData := [][]byte{fields[string(field)], bytes.TrimSpace(brokenLineRegex.FindSubmatch(line)[1])}
-				fields[string(field)] = bytes.Join(byteData, []byte(" "))
+				fields[string(field)] = bytes.Join(byteData, dataJoin)
 			}
 		} else {
-			byteData := [][]byte{fields["contents"], line, []byte("\n")}
-			fields["contents"] = bytes.Join(byteData, []byte(""))
+			byteData := [][]byte{fields["contents"], line, dataNewLine}
+			fields["contents"] = bytes.Join(byteData, emptyByte)
 		}
 	}
 
@@ -57,12 +63,12 @@ func Process(fields map[string][]byte) map[string][]byte {
 	if messageId != nil {
 		fields["message_id"] = messageId[1]
 	}
-	contentTypes := bytes.Split(fields["content_type"], []byte(";"))
+	contentTypes := bytes.Split(fields["content_type"], contentTypeSep)
 	if len(contentTypes) > 1 {
 		fields["content_type"] = contentTypes[0]
-		fields["charset"] = bytes.Split(contentTypes[1], []byte("="))[1]
+		fields["charset"] = bytes.Split(contentTypes[1], charsetSep)[1]
 	}
-	fields["x_folder"] = bytes.ReplaceAll(fields["x_folder"], []byte("\\"), []byte("/"))
+	fields["x_folder"] = bytes.ReplaceAll(fields["x_folder"], oldPathSep, newPathSep)
 
 	return fields
 }
