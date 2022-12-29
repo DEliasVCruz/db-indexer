d6614c848b97e1082c5edd8815c79a3a26e16c7c
DEliasVilelaC <danielvilelayo@gmail.com>
2022-12-29 05:20:18 -0500

    feat(frontend): add global footer template

---
+++ frontend/src/components/TheFooter.vue
@@ -0,0 +1,43 @@
+<template>
+  <footer class="absolute bottom-0 w-full bg-gray-900 pb-6">
+    <div class="container mx-auto px-4">
+      <div
+        class="flex flex-wrap items-center justify-center md:justify-between"
+      >
+        <div class="w-full px-4 md:w-4/12">
+          <p class="py-1 text-sm font-semibold text-white">
+            Diseñado y programado por
+            <a href="https://github.com/DEliasVCruz">Daniel Viela</a>
+          </p>
+        </div>
+        <div class="w-full px-4 md:w-8/12">
+          <ul class="flex list-none flex-wrap justify-center md:justify-end">
+            <li>
+              <a
+                href="/about.html"
+                class="block py-1 px-3 text-sm font-semibold text-white hover:text-gray-400"
+                >About</a
+              >
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/daniel-vilela-cruz-694968138/"
+                class="block py-1 px-3 text-sm font-semibold text-white hover:text-gray-400"
+                >Contact</a
+              >
+            </li>
+            <li>
+              <a
+                href="https://github.com/DEliasVCruz/db-indexer"
+                class="block py-1 px-3 text-sm font-semibold text-white hover:text-gray-400"
+                >Fork Me!</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
