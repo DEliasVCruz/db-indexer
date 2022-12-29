2913178ec77e093200e7c9649b7fb6d008def421
DEliasVilelaC <danielvilelayo@gmail.com>
2022-12-29 05:21:37 -0500

    feat(frontend): add landing header component

---
+++ frontend/src/components/LandingHeader.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts"></script>
+
+<template>
+  <!-- <header class="absolute top-0 w-full bg-gray-900 pb-6"></header> -->
+  <header
+    class="relative h-72 w-full overflow-hidden align-middle leading-none"
+  >
+    <svg
+      data-name="Layer 1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1200 120"
+      preserveAspectRatio="none"
+      class="absolute inset-x-0 top-0 block h-72"
+    >
+      <path d="M1200 0L0 0 598.97 114.72 1200 0z" class="fill-black-500"></path>
+    </svg>
+    <svg
+      data-name="Layer 1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 1200 120"
+      preserveAspectRatio="none"
+      class="absolute inset-x-0 -top-6 block h-72"
+    >
+      <path d="M1200 0L0 0 598.97 114.72 1200 0z" class="fill-red-500"></path>
+    </svg>
+    <div class="inline-grid h-40 w-full grid-cols-3 grid-rows-3 md:h-72">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <h1 class="absoulte h-32 text-center text-4xl text-yellow-200">
+        Mailing Directory Indexer
+      </h1>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="postcss">
+svg {
+  width: calc(100% + 1.3px);
+  z-index: -1;
+}
+</style>
