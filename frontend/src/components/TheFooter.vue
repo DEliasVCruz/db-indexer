619de3e9cd4d1f7234f58881175e1151d3bf81de
DEliasVilelaC <danielvilelayo@gmail.com>
2022-12-29 07:45:51 -0500

    refactor(frontend): simplify footer component

--- frontend/src/components/TheFooter.vue
+++ frontend/src/components/TheFooter.vue
@@ -1,43 +1,38 @@
 <template>
-  <footer class="absolute bottom-0 w-full bg-gray-900 pb-6">
-    <div class="container mx-auto px-4">
-      <div
-        class="flex flex-wrap items-center justify-center md:justify-between"
+  <footer class="relative mt-auto w-screen bg-gray-900 py-6">
+    <p
+      class="absolute bottom-0 -left-1 m-1 w-screen p-0.5 text-center text-xs font-semibold text-gray-400 sm:left-0 sm:p-1 sm:text-left"
+    >
+      <!-- class="absolute bottom-0 -left-1 m-1 w-screen p-0.5 text-center text-xs font-semibold text-gray-500" -->
+      Diseñado y desarrollado por
+      <a
+        href="https://github.com/DEliasVCruz"
+        class="font-bold hover:font-extrabold hover:text-white md:hover:absolute md:hover:bottom-2 md:hover:ml-0.5"
+        >Daniel Viela</a
       >
-        <div class="w-full px-4 md:w-4/12">
-          <p class="py-1 text-sm font-semibold text-white">
-            Diseñado y programado por
-            <a href="https://github.com/DEliasVCruz">Daniel Viela</a>
-          </p>
-        </div>
-        <div class="w-full px-4 md:w-8/12">
-          <ul class="flex list-none flex-wrap justify-center md:justify-end">
-            <li>
-              <a
-                href="/about.html"
-                class="block py-1 px-3 text-sm font-semibold text-white hover:text-gray-400"
-                >About</a
-              >
-            </li>
-            <li>
-              <a
-                href="https://www.linkedin.com/in/daniel-vilela-cruz-694968138/"
-                class="block py-1 px-3 text-sm font-semibold text-white hover:text-gray-400"
-                >Contact</a
-              >
-            </li>
-            <li>
-              <a
-                href="https://github.com/DEliasVCruz/db-indexer"
-                class="block py-1 px-3 text-sm font-semibold text-white hover:text-gray-400"
-                >Fork Me!</a
-              >
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    </p>
+    <ul class="flex flex-row flex-wrap justify-center md:justify-end">
+      <li>
+        <a
+          href="/about.html"
+          class="py-1 px-3 text-sm font-semibold text-white hover:text-gray-400"
+          >About</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/daniel-vilela-cruz-694968138/"
+          class="py-1 px-3 text-sm font-semibold text-white hover:text-gray-400"
+          >Contact</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/DEliasVCruz/db-indexer"
+          class="py-1 px-3 text-sm font-semibold text-white hover:text-gray-400"
+          >Fork Me!</a
+        >
+      </li>
+    </ul>
   </footer>
 </template>
-
-<script setup lang="ts"></script>
