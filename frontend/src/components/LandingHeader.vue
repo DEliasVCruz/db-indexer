cc6bec90dbcc3e598e18d75e776fc324df4eaf96
DEliasVilelaC <danielvilelayo@gmail.com>
2022-12-29 07:38:31 -0500

    refactor(frontend): make minor adjustments to landing header

--- frontend/src/components/LandingHeader.vue
+++ frontend/src/components/LandingHeader.vue
@@ -3,7 +3,7 @@
 <template>
   <!-- <header class="absolute top-0 w-full bg-gray-900 pb-6"></header> -->
   <header
-    class="relative h-72 w-full overflow-hidden align-middle leading-none"
+    class="relative h-72 w-screen overflow-hidden align-middle leading-none"
   >
     <svg
       data-name="Layer 1"
@@ -23,12 +23,12 @@
     >
       <path d="M1200 0L0 0 598.97 114.72 1200 0z" class="fill-red-500"></path>
     </svg>
-    <div class="inline-grid h-40 w-full grid-cols-3 grid-rows-3 md:h-72">
+    <div class="inline-grid h-10 w-full grid-cols-3 grid-rows-3 md:h-40">
       <span></span>
       <span></span>
       <span></span>
       <span></span>
-      <h1 class="absoulte h-32 text-center text-4xl text-yellow-200">
+      <h1 class="absoulte h-32 py-6 text-center text-4xl text-yellow-200">
         Mailing Directory Indexer
       </h1>
     </div>
