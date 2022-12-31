413b103b4bfb758a9b84548ddafcab7e72617f89
DEliasVilelaC <danielvilelayo@gmail.com>
2022-12-31 09:31:07 -0500

    feat(frontend): add sidebar option component

---
+++ frontend/src/components/SideBarOption.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const props = defineProps<{
+  tooltipText: string;
+}>();
+</script>
+
+<template>
+  <div class="sidebar-option group">
+    <slot />
+    <span class="sidebar-tooltip group-hover:scale-100">
+      {{ props.tooltipText }}
+    </span>
+  </div>
+</template>
+
+<style scoped></style>
