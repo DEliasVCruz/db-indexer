b2306129ea42207868d20db014c978c3cdd90dfe
DEliasVilelaC <danielvilelayo@gmail.com>
2022-12-31 09:20:46 -0500

    feat(frontend): add basic sidebar rendering

---
+++ frontend/src/components/SideBar.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import SideBarOption from "./SideBarOption.vue";
+import IconDirectory from "./IconDirectory.vue";
+import IconIndex from "./IconIndex.vue";
+import { reactive } from "vue";
+
+const options = reactive([
+  { component: IconIndex, name: "Index" },
+  { component: IconDirectory, name: "Dirs" },
+]);
+</script>
+
+<template>
+  <div
+    class="left-0 m-0 flex h-[87vh] w-16 flex-col bg-red-500 text-white shadow-lg"
+  >
+    <SideBarOption
+      v-for="option in options"
+      :key="option.name"
+      :tooltip-text="option.name"
+    >
+      <component :is="option.component"></component>
+    </SideBarOption>
+  </div>
+</template>
+
+<style scoped></style>
