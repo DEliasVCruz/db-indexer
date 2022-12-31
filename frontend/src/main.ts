0ca3e531c4f1c892bb20cf7900048dcb2d1e0609
DEliasVilelaC <danielvilelayo@gmail.com>
2022-12-31 09:22:49 -0500

    feat(frontend): mount apps in main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from "vue";
 import LandingApp from "./app/LandingApp.vue";
 import AboutPage from "./app/AboutPage.vue";
+import DirectoryApp from "./app/DirectoryApp.vue";
 import "./index.css";
 
 const landing = createApp(LandingApp);
 const about = createApp(AboutPage);
+const maildir = createApp(DirectoryApp);
 
-landing.mount("#app");
+landing.mount("#landing");
 about.mount("#about");
+maildir.mount("#maildir");
