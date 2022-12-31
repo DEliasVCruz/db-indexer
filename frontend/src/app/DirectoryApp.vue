07aa8c1a14eb3ee74de2c3a51753bbd9b05cdd4b
DEliasVilelaC <danielvilelayo@gmail.com>
2022-12-31 09:23:27 -0500

    feat(frontend): add directory app main layout

--- frontend/src/app/DirectoryApp.vue
+++ frontend/src/app/DirectoryApp.vue
@@ -1,7 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TheFooter from "@/components/TheFooter.vue";
+import TheHeader from "@/components/TheHeader.vue";
+import SideBar from "@/components/SideBar.vue";
+import ResultTable from "@/components/ResultTable.vue";
+</script>
 
 <template>
-  <div></div>
+  <div class="outer-layer">
+    <TheHeader :search-bar="true" />
+    <main flex flex-row>
+      <SideBar />
+      <ResultTable />
+    </main>
+    <TheFooter />
+  </div>
 </template>
 
 <style scoped></style>
