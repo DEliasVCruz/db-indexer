9092ca71f559b53a5c0787a22597b9fdeeb8ae5a
DEliasVilelaC <danielvilelayo@gmail.com>
2022-12-31 09:29:03 -0500

    refactor(frontend): change svg descriptions

--- frontend/src/components/TheHeader.vue
+++ frontend/src/components/TheHeader.vue
@@ -7,30 +7,28 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="relative">
-    <header class="fixed top-0 left-0 right-0 h-14 bg-blue-900 py-6">
-      <div class="absolute top-0 left-0 flex h-14 flex-row gap-x-5">
-        <div class="flex flex-row">
+  <header class="top-0 left-0 right-0 h-[7vh] bg-blue-900 py-6">
+    <div class="absolute top-0 left-0 flex h-14 flex-row gap-x-5">
+      <div class="flex flex-row">
+        <img
+          class="h-14 w-14"
+          src="../assets/menu.svg"
+          alt="icon to toggle nav bar"
+        />
+        <span class="-ml-1 mb-1 inline-flex items-center gap-x-1">
           <img
-            class="h-14 w-14"
-            src="../assets/menu.svg"
-            alt="icon to toggle nav bar"
+            class="hidden h-6 w-6 fill-none sm:inline-block"
+            src="../assets/logo.svg"
+            alt="page logo in the shape of an @ symbol"
           />
-          <span class="-ml-1 mb-1 inline-flex items-center gap-x-1">
-            <img
-              class="hidden h-6 w-6 fill-none sm:inline-block"
-              src="../assets/logo.svg"
-              alt="page logo in the shape of an @ symbol"
-            />
-            <h1>MailInDexer</h1>
-          </span>
-        </div>
-        <div v-if="props.searchBar">
-          <TheSearchBar />
-        </div>
+          <h1>MailInDexer</h1>
+        </span>
       </div>
-    </header>
-  </div>
+      <div v-if="props.searchBar">
+        <TheSearchBar />
+      </div>
+    </div>
+  </header>
 </template>
 
 <style scoped lang="postcss">
