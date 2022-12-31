8234871368a737cf06372e33c243bac35c2a0f92
DEliasVilelaC <danielvilelayo@gmail.com>
2022-12-31 09:30:33 -0500

    chore(frontend): add tablecolumn app template

---
+++ frontend/src/components/TableColumn.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import TableCell from "./TableCell.vue";
+
+const props = defineProps<{
+  data: string[];
+}>();
+</script>
+
+<template>
+  <div>
+    <TableCell v-for="value in props.data" :key="props.data.indexOf(value)" />
+  </div>
+</template>
+
+<style scoped></style>
