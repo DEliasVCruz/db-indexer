fb5c6987be87629ecd220160e76afb0cd4819867
DEliasVilelaC <danielvilelayo@gmail.com>
2023-01-02 14:39:54 -0500

    fix(frontend): streamline header layout

--- frontend/src/components/TheHeader.vue
+++ frontend/src/components/TheHeader.vue
@@ -7,25 +7,49 @@ const props = defineProps<{
 </script>
 
 <template>
-  <header class="top-0 left-0 right-0 h-[7vh] bg-blue-900 py-6">
-    <div class="absolute top-0 left-0 flex h-14 flex-row gap-x-5">
-      <div class="flex flex-row">
-        <img
-          class="h-14 w-14"
-          src="../assets/menu.svg"
-          alt="icon to toggle nav bar"
-        />
-        <span class="-ml-1 mb-1 inline-flex items-center gap-x-1">
-          <img
-            class="hidden h-6 w-6 fill-none sm:inline-block"
-            src="../assets/logo.svg"
-            alt="page logo in the shape of an @ symbol"
-          />
-          <h1>MailInDexer</h1>
-        </span>
-      </div>
-      <div v-if="props.searchBar">
-        <TheSearchBar />
+  <header class="top-0 left-0 right-0 h-[8.5vh] w-screen bg-blue-900">
+    <div class="flex h-full w-full flex-row p-2 sm:gap-x-5">
+      <div class="flex h-full grow flex-row gap-x-2 sm:grow-0">
+        <div
+          class="w-full rounded-full bg-gray-200 bg-opacity-0 pt-3 hover:bg-opacity-60 sm:w-fit sm:p-3"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="h-6 w-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </div>
+        <div class="inline-flex items-center gap-x-1">
+          <div class="hidden sm:inline-block">
+            <svg
+              class="h-7 w-7"
+              fill="none"
+              viewBox="0 0 48 48"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0 24C0 10.7452 10.7452 0 24 0C37.2548 0 48 10.7452 48 24C48 37.2548 37.2548 48 24 48C10.7452 48 0 37.2548 0 24Z"
+                fill="#005FF9"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M10 23.9603C10 16.2625 16.2802 10 23.9998 10C31.7194 10 37.9997 16.2625 37.9997 23.9603C37.9997 25.0508 37.9087 25.9336 37.7048 26.8197L37.7018 26.8344C37.7013 26.837 37.6001 27.2455 37.5442 27.4237C37.1982 28.5272 36.5718 29.4463 35.7325 30.0815C34.9186 30.6977 33.9094 31.0371 32.891 31.0371C32.7652 31.0371 32.6385 31.032 32.5144 31.0221C31.0682 30.9063 29.7966 30.1474 29.0168 28.9373C27.6757 30.282 25.8964 31.0218 23.9998 31.0218C20.0951 31.0218 16.9184 27.854 16.9184 23.9603C16.9184 20.0666 20.0951 16.8989 23.9998 16.8989C27.9045 16.8989 31.0812 20.0666 31.0812 23.9603V26.2366C31.087 27.5648 31.9808 28.1077 32.7444 28.1691C33.5041 28.2276 34.5152 27.7863 34.8674 26.3461C35.0411 25.5562 35.1294 24.7533 35.1294 23.9603C35.1294 17.8407 30.1367 12.8621 23.9998 12.8621C17.8629 12.8621 12.8703 17.8407 12.8703 23.9603C12.8703 30.0798 17.8629 35.0585 23.9998 35.0585C26.136 35.0585 28.2136 34.4501 30.0079 33.2991L30.0401 33.2784L31.9261 35.465L31.8856 35.4926C29.5549 37.0811 26.828 37.9207 23.9998 37.9207C16.2802 37.9207 10 31.6581 10 23.9603ZM23.9998 28.1596C26.3218 28.1596 28.2109 26.2758 28.2109 23.9603C28.2109 21.6448 26.3218 19.7612 23.9998 19.7612C21.6777 19.7612 19.7887 21.6448 19.7887 23.9603C19.7887 26.2758 21.6777 28.1596 23.9998 28.1596Z"
+                fill="#FF9E00"
+              />
+            </svg>
+          </div>
+          <h1 class="mx-2 hidden sm:inline-block">MailInDir</h1>
+        </div>
       </div>
     </div>
   </header>
