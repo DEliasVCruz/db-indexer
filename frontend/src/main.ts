75fdcaefb7f5fe53cbcdc82c9bc68fed622b09c6
DEliasVilelaC <danielvilelayo@gmail.com>
2023-01-02 14:33:05 -0500

    chore(frontend): change main index app reference

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,12 @@
 import { createApp } from "vue";
 import LandingApp from "./app/LandingApp.vue";
 import AboutPage from "./app/AboutPage.vue";
-import DirectoryApp from "./app/DirectoryApp.vue";
+import IndexExplorer from "./app/IndexExplorer.vue";
 import "./index.css";
 
 const landing = createApp(LandingApp);
 const about = createApp(AboutPage);
-const maildir = createApp(DirectoryApp);
+const maildir = createApp(IndexExplorer);
 
 landing.mount("#landing");
 about.mount("#about");
