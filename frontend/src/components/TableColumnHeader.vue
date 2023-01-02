7f0e429fc363743b3c503767c6088c81b8d8ac9a
DEliasVilelaC <danielvilelayo@gmail.com>
2023-01-02 14:29:25 -0500

    feat(frontend): add table column header component

---
+++ frontend/src/components/TableColumnHeader.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { column } from "../globals/directory";
+
+const props = defineProps<{
+  title: string;
+}>();
+
+const selected = computed(() => props.title === column.selected);
+</script>
+
+<template>
+  <div
+    class="mt-2 ml-0 flex flex-col border-b-2"
+    @click.passive="column.select(props.title)"
+  >
+    <div class="py-2 px-1 text-left after:h-3 after:bg-blue-200">
+      {{ props.title }}
+    </div>
+    <span
+      aria-hidden="true"
+      class="block h-1 w-full group-active:bg-blue-500"
+      :class="{ 'bg-blue-500': selected }"
+    ></span>
+  </div>
+</template>
+
+<style scoped></style>
