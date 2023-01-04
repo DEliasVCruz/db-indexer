0820d399bd2e91396626551f8a3c187a6cf62192
DEliasVilelaC <danielvilelayo@gmail.com>
2023-01-04 00:15:52 -0500

    feat(frontend): add global table data state

--- frontend/src/globals/table.ts
+++ frontend/src/globals/table.ts
@@ -1,4 +1,10 @@
 import { reactive } from "vue";
+import type { ColumnData } from "@/globals/types";
+
+interface Columns {
+  columns: Array<ColumnData>;
+  set(columns: Array<ColumnData>): void;
+}
 
 export const column = reactive({
   selected: "Contents",
@@ -11,5 +17,13 @@ export const row = reactive({
   hovered: 0,
   hover(row: number) {
     this.hovered = row;
+  },
+});
+
+export const columnData: Columns = reactive({
+  columns: new Array(),
+  set(columns: Array<ColumnData>) {
+    this.columns.length = 0;
+    this.columns = columns;
   },
 });
