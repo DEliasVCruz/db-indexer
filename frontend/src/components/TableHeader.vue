daad25275970b9f716fae6bc4d180abf47f59554
DEliasVilelaC <danielvilelayo@gmail.com>
2023-01-04 02:41:26 -0500

    fet(frontend): add separate header componenta

---
+++ frontend/src/components/TableHeader.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import TableColumnHeader from "./TableColumnHeader.vue";
+
+export interface Props {
+  columns?: { name: string; values: string[] }[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  columns: () => [
+    { name: "From", values: ["Empty", "Empty", "Empty"] },
+    { name: "Subject", values: ["Empty", "Empty", "Empty"] },
+    { name: "Contents", values: ["Empty", "Empty", "Empty"] },
+  ],
+});
+</script>
+
+<template>
+  <div class="flex min-w-max cursor-pointer flex-row">
+    <TableColumnHeader
+      v-for="column in props.columns"
+      :key="column.name"
+      :title="column.name"
+    />
+  </div>
+</template>
