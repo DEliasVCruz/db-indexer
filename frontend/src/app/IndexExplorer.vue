5d7abef713c56747fb8f14070472bf35e840a5be
DEliasVilelaC <danielvilelayo@gmail.com>
2023-01-04 00:16:55 -0500

    refactor(frontend): add table component with global state

--- frontend/src/app/IndexExplorer.vue
+++ frontend/src/app/IndexExplorer.vue
@@ -3,15 +3,17 @@ import TheFooter from "@/components/TheFooter.vue";
 import TheHeader from "@/components/TheHeader.vue";
 import SideBar from "@/components/SideBar.vue";
 import ResultTable from "@/components/ResultTable.vue";
+import { columnData } from "@/globals/table";
 </script>
 
 <template>
   <div class="outer-layer">
     <TheHeader :search-bar="true" />
-    <div class="flex h-[85.5vh] w-screen flex-row gap-4">
+    <div class="flex h-[85.5vh] w-screen flex-row">
       <SideBar />
-      <main class="w-[90vw]">
-        <ResultTable />
+      <main class="z-0 w-[94%] flex-initial">
+        <!-- <ResultTable /> -->
+        <ResultTable :columns="columnData.columns" />
       </main>
     </div>
     <TheFooter />
