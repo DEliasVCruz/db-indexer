c5ea394331d6d92a59303a313af8a9d69a2fdf1f
DEliasVilelaC <danielvilelayo@gmail.com>
2023-01-07 17:36:30 -0500

    feat(frontend): add mail viewer

---
+++ frontend/src/components/MailView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { row } from "@/globals/table";
+</script>
+
+<template>
+  <div class="h-full w-full whitespace-pre-wrap">
+    {{ row.data.get("Contents") }}
+  </div>
+</template>
