9a8f652a3c3bf7834b6bd5eb7ed07049cb4ba7e9
DEliasVilelaC <danielvilelayo@gmail.com>
2023-01-07 22:38:37 -0500

    feat(frontend): add main content caching

--- frontend/src/app/IndexExplorer.vue
+++ frontend/src/app/IndexExplorer.vue
@@ -4,10 +4,12 @@ import TheHeader from "@/components/TheHeader.vue";
 import ToolBar from "@/components/ToolBar.vue";
 import SideBar from "@/components/SideBar.vue";
 import ResultTable from "@/components/ResultTable.vue";
+import MailView from "@/components/MailView.vue";
 import { mainContent } from "@/globals/content";
 
 const contents = {
   ResultTable,
+  MailView,
 };
 </script>
 
@@ -19,9 +21,11 @@ const contents = {
       <main class="z-0 flex w-[94%] flex-initial flex-col pb-3 pt-1 pl-6">
         <ToolBar />
         <div class="h-[94%] overflow-x-auto">
-          <component
-            :is="contents[mainContent.current as keyof typeof contents]"
-          ></component>
+          <KeepAlive :max="2">
+            <component
+              :is="contents[mainContent.current as keyof typeof contents]"
+            ></component>
+          </KeepAlive>
         </div>
       </main>
     </div>
