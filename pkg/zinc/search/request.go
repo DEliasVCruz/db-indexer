6de3740b415369a4248348886467df9b55a1d20b
DEliasVilelaC <danielvilelayo@gmail.com>
2023-01-08 17:38:44 -0500

    refactor: restructure search query struct

--- pkg/zinc/search/request.go
+++ pkg/zinc/search/request.go
@@ -1,11 +1,20 @@
 package search
 
-type Query struct {
+type SearchQuery struct {
 	From  int        `json:"from"`
 	Size  int        `json:"size"`
 	Query *QueryType `json:"query"`
 }
 
 type QueryType struct {
-	Match map[string]map[string]string `json:"match"`
+	Match map[string]*Query `json:"match,omitempty"`
+	Bool  *QueryBool        `json:"bool,omitempty"`
+}
+
+type Query struct {
+	Text string `json:"query"`
+}
+
+type QueryBool struct {
+	Must []*QueryType `json:"must"`
 }
