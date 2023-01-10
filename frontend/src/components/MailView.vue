fe718f975bb81152f140d1ed767c5ed31ef6c593
DEliasVilelaC <danielvilelayo@gmail.com>
2023-01-10 10:49:36 -0500

    feat(frontend): add left margin to mail viewer

--- frontend/src/components/MailView.vue
+++ frontend/src/components/MailView.vue
@@ -3,7 +3,10 @@ import { row } from "@/globals/table";
 </script>
 
 <template>
-  <div class="h-full w-full whitespace-pre-wrap">
-    {{ row.data.get("Contents") }}
+  <div class="flex flex-row">
+    <div class="w-[3%]"></div>
+    <p class="h-full w-full whitespace-pre-wrap">
+      {{ row.data.get("Contents") }}
+    </p>
   </div>
 </template>
