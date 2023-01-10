a430de1e8125302076c1a5e2868eb8960e737ac9
DEliasVilelaC <danielvilelayo@gmail.com>
2023-01-10 15:54:22 -0500

    refactor: simplify cors middleware

--- pkg/handlers/middleware.go
+++ pkg/handlers/middleware.go
@@ -1,21 +1,26 @@
 package handlers
 
 import (
-	"log"
 	"net/http"
 )
 
 func Cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
+		headers := w.Header()
+
 		if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
 
-			handlePreflight(w, r)
+			headers.Add("Vary", "Origin")
+			headers.Add("Vary", "Access-Control-Request-Method")
+			headers.Add("Vary", "Access-Control-Request-Headers")
+
+			addCors(headers)
 			w.WriteHeader(http.StatusOK)
 
 		} else {
 
-			handleNormalRequest(w, r)
+			addCors(headers)
 			next.ServeHTTP(w, r)
 
 		}
@@ -27,37 +32,4 @@ func addCors(headers http.Header) {
 	headers.Add("Access-Control-Allow-Origin", "*")
 	headers.Add("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,HEAD,OPTIONS")
 	headers.Add("Access-Control-Allow-Headers", "Content-Type, Origin, Accept, Authorization, Content-Length, X-Requested-With")
-}
-
-func handleNormalRequest(w http.ResponseWriter, r *http.Request) {
-
-	headers := w.Header()
-	origin := r.Header.Get("Origin")
-
-	headers.Add("Vary", "Origin")
-	if origin == "" {
-		log.Println("No provided origin")
-		return
-	}
-
-	addCors(headers)
-
-}
-
-func handlePreflight(w http.ResponseWriter, r *http.Request) {
-
-	headers := w.Header()
-	origin := r.Header.Get("Origin")
-
-	if origin == "" {
-		log.Println("No provided origin")
-		return
-	}
-
-	headers.Add("Vary", "Origin")
-	headers.Add("Vary", "Access-Control-Request-Method")
-	headers.Add("Vary", "Access-Control-Request-Headers")
-
-	addCors(headers)
-
 }
