233f18839519ca5b84dcfa3dd4e7e873b5b6c710
DEliasVilelaC <danielvilelayo@gmail.com>
2023-01-10 13:17:34 -0500

    chore(frontend): change main css name

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import LandingApp from "./app/LandingApp.vue";
 import AboutPage from "./app/AboutPage.vue";
 import IndexExplorer from "./app/IndexExplorer.vue";
-import "./index.css";
+import "./main.css";
 
 const landing = createApp(LandingApp);
 const about = createApp(AboutPage);
