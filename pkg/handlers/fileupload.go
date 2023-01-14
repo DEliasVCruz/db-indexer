8657f21ad692cedf0bd428f7eb82d71e21200f3f
DEliasVilelaC <danielvilelayo@gmail.com>
2023-01-14 16:22:37 -0500

    feat: add index creation in upload

--- pkg/handlers/fileupload.go
+++ pkg/handlers/fileupload.go
@@ -5,35 +5,41 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strings"
+
+	"github.com/DEliasVCruz/db-indexer/pkg/data"
+	"github.com/DEliasVCruz/db-indexer/pkg/index"
 )
 
-const MAX_UPLOAD_SIZE = 1024 * 1024 * 500 // 500MB
+const MAX_UPLOAD_SIZE = 500<<20 + 1024 // 500MB
+const MAX_MEMORY = 20 << 20            // 20MB
 
 func FileUpload(w http.ResponseWriter, r *http.Request) {
 
 	r.Body = http.MaxBytesReader(w, r.Body, MAX_UPLOAD_SIZE)
 
-	if err := r.ParseMultipartForm(MAX_UPLOAD_SIZE); err != nil {
+	if err := r.ParseMultipartForm(MAX_MEMORY); err != nil {
 		http.Error(w, "file exceed max upload size", http.StatusBadRequest)
 		return
 	}
 
-	file, _, err := r.FormFile("file")
+	file, fh, err := r.FormFile("file")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	defer file.Close()
 
 	buff := make([]byte, 512)
 	_, err = file.Read(buff)
 	if err != nil {
+		file.Close()
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	_, err = file.Seek(0, io.SeekStart)
 	if err != nil {
+		file.Close()
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -45,6 +51,7 @@ func FileUpload(w http.ResponseWriter, r *http.Request) {
 
 		_, err := tr.Next()
 		if err != nil {
+			file.Close()
 			http.Error(w, "the provided file format is not allowed", http.StatusBadRequest)
 			return
 		}
@@ -52,9 +59,17 @@ func FileUpload(w http.ResponseWriter, r *http.Request) {
 		filetype = "application/tar"
 	}
 
+	go index.NewIndex(
+		"My Index",
+		strings.TrimPrefix(filetype, "application/"),
+		"",
+		&data.FormFile{File: file, Size: fh.Size},
+	)
+
 	response, err := json.Marshal(&FileUploaded{Uploaded: true, State: "processing"})
 	if err != nil {
 		http.Error(w, "server marshaling failed", http.StatusInternalServerError)
+		return
 	}
 
 	w.Write(response)
